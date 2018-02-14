25e55b15161cca683d60a50a99537b18136f554b
Alexander Taylor <Alexander Taylor>
2018-02-14 17:29:42 -0500

    Add project files.

---
+++ ProHookup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProHookup.MvcApplication" Language="C#" %>
---
+++ ProHookup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProHookup.Startup))]
+namespace ProHookup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
